bf14e22ca3d6589d32f85b7a52d529a1783c48b0
Viktor Cherniy <vikchernij@gmail.com>
2013-10-12 01:25:34 +0300

    Added rules for parsers tests

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -8,14 +8,52 @@ class Test_Saw(unittest.TestCase):
         pass
 
     def test_blocks(self):
-        res = Blocks.parse('John, stand up!')
-        self.assertEqual(res, [['John', ','], ['stand up!', '']])
+        single_rules = [',', ':', '=', '+', ';', '*']
+        for rule in single_rules:
+            """
+            Test with:
+            space after, space before, space around, another symbols
+            few spaces - to one space
+            """
+            t_rule = tuple([rule])
+            text = "This example%s  with %s anoher. comb!  of %sspaces" % [rule] * 3
+            expect = ['This example', t_rule, 'with', t_rule, 'another. comb!  of', t_rule, 'spaces']
+            self.assertEqual(Blocks.parse(text), expect)
 
-        res = Blocks.parse('Enter that: pin code.')
-        self.assertEqual(res, [['Enter that', ':'], ['pin code.', '']])
+            """
+            Test with:
+            dublicated symbols, another symbol beetwen dublicated (or from this range)
+            symbol in end of string - not empty value
+            """
+            d_rule = tuple([rule, rule])
+            d_with_mixin = tuple([rule, ',', rule])
+            text = "This example%s%s with %s,%s anoher %s.%s spaces%s" % [rule] * 7
+            expect = ['This example', d_rule, 'with', d_with_mixin, 'another',  t_rule, '.', t_rule, 'spaces', t_rule]
+            self.assertEqual(Blocks.parse(text), expect)
 
-        res = Blocks.parse('Limite it:: hey,')
-        self.assertEqual(res, [['Limite it', '::'], ['hey', ',']])
+            #@TODO: "-"
+
+        double_rule_begin = ['{', '(', '[', '"', "'"]
+        double_rule_end   = ['}', ')', ']', '"', "'"]
+        
+        double_rules = zip(double_rule_begin, double_rule_end)
+
+        for rule in double_rules:
+            
+
+
+
+        res = Blocks.parse('Nine - number')
+        self.assertEqual(res, ['Nine', ('-'), 'number'])
+
+        res = Blocks.parse('Cat (or dog) name')
+        self.assertEqual(res, ['cat', ('('), 'or dog', (')'), 'name')
+
+        res = Blocks.parse('Cat (or dog (or poni)) name')
+        self.assertEqual(res, ['cat', ('('), 'or dog', ('('), 'or poni', (')', ')'), 'name')
+
+        res = Blocks.parse('The Car "Micro" fired')
+        self.assertEqual(res, ['The Car', ('"'), 'Micro', ('"'), 'fired'])
 
 if __name__ == "__main__":
     unittest.main()
