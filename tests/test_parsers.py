ebc1914edf15c9f2b907a4df0017a98529a01dd9
Viktor Cherniy <vikchernij@gmail.com>
2013-10-13 20:26:34 +0300

    Add paragraphs parser tests

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -3,6 +3,7 @@ import unittest
 from saw.parser.blocks import Blocks
 from saw.parser.words import Words
 from saw.parser.sentences import Sentences
+from saw.parser.paragraphs import Paragraphs
 
 class Test_Saw(unittest.TestCase):
 
@@ -30,8 +31,8 @@ class Test_Saw(unittest.TestCase):
             dublicated symbols, another symbol beetwen dublicated (or from this range)
             symbol in end of string - not empty value
             """
-            text = "This example%s%s   with. %s,%s anoher %s.%s spaces%s" % [rule] * 7
-            expect = ['This example', [srule, srule], 'with.', [srule, ',', srule], 'another',  [srule], '.', [srule], 'spaces', [srule]]
+            text = "%s This example%s%s   with. %s,%s anoher %s.%s spaces%s" % [rule] * 8
+            expect = [[srule], 'This example', [srule, srule], 'with.', [srule, ',', srule], 'another',  [srule], '.', [srule], 'spaces', [srule]]
             self.assertEqual(Blocks.parse(text), expect)
 
     def test_words(self):
@@ -43,6 +44,11 @@ class Test_Saw(unittest.TestCase):
         text = '1 sentence! But what!!WTF ? That 12.45 points. '
         expect = ['1 sentence', ['!'], 'But what', ['!', '!'], 'WTF', ['?'], 'That 12.45 points', ['.']]
         self.assertEqual(Sentences.parse(text), expect)
+
+    def test_paragraphs(self):
+        text = "Hi. Hola. \n These items:\n- First item,\n- Second item,   \n - Third item  .\n\nEnded\n"
+        expect = ['Hi. Hola.', ["\n"], 'These items:', ["\n"], '- First item,', ["\n"], '- Second item,', ["\n"], '- Third item  .', ["\n", "\n"], "Ended", ["\n"]]
+        self.assertEqual(Paragraphs.parse(text), expect)
 
 
 if __name__ == "__main__":
