2a605ab02b0579418a012ff5e87c47b42ca95297
Viktor Cherniy <vikchernij@gmail.com>
2013-10-13 19:44:15 +0300

    Change tuple in parser's result to list

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -15,9 +15,8 @@ class Test_Saw(unittest.TestCase):
             space after, space before, space around, another symbols
             few spaces - to one space
             """
-            t_rule = tuple([rule])
             text = "This example%s  with %s anoher. comb!  of %sspaces" % [rule] * 3
-            expect = ['This example', t_rule, 'with', t_rule, 'another. comb!  of', t_rule, 'spaces']
+            expect = ['This example', [rule], 'with', [rule], 'another. comb!  of', [rule], 'spaces']
             self.assertEqual(Blocks.parse(text), expect)
 
             """
@@ -25,10 +24,10 @@ class Test_Saw(unittest.TestCase):
             dublicated symbols, another symbol beetwen dublicated (or from this range)
             symbol in end of string - not empty value
             """
-            d_rule = tuple([rule, rule])
-            d_with_mixin = tuple([rule, ',', rule])
+            d_rule = [rule, rule]
+            d_with_mixin = [rule, ',', rule]
             text = "This example%s%s with %s,%s anoher %s.%s spaces%s" % [rule] * 7
-            expect = ['This example', d_rule, 'with', d_with_mixin, 'another',  t_rule, '.', t_rule, 'spaces', t_rule]
+            expect = ['This example', d_rule, 'with', d_with_mixin, 'another',  [rule], '.', [rule], 'spaces', [rule]]
             self.assertEqual(Blocks.parse(text), expect)
 
             #@TODO: "-"
