d70dcd090be2176fef61680b61b2e788145f0f56
Viktor Cherniy <vikchernij@gmail.com>
2013-10-13 20:06:24 +0300

    Add tests for words parser

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,6 +1,7 @@
 import unittest
 
 from saw.parser.blocks import Blocks
+from saw.parser.words import Words
 
 class Test_Saw(unittest.TestCase):
 
@@ -19,7 +20,7 @@ class Test_Saw(unittest.TestCase):
             space after, space before, space around, another symbols
             few spaces - to one space
             """
-            text = "This example%s  with %s anoher. comb!  of %sspaces" % [rule] * 3
+            text = "This example%s   with %s anoher. comb!  of %sspaces" % [rule] * 3
             expect = ['This example', [srule], 'with', [srule], 'another. comb!  of', [srule], 'spaces']
             self.assertEqual(Blocks.parse(text), expect)
 
@@ -28,9 +29,16 @@ class Test_Saw(unittest.TestCase):
             dublicated symbols, another symbol beetwen dublicated (or from this range)
             symbol in end of string - not empty value
             """
-            text = "This example%s%s with. %s,%s anoher %s.%s spaces%s" % [rule] * 7
+            text = "This example%s%s   with. %s,%s anoher %s.%s spaces%s" % [rule] * 7
             expect = ['This example', [srule, srule], 'with.', [srule, ',', srule], 'another',  [srule], '.', [srule], 'spaces', [srule]]
             self.assertEqual(Blocks.parse(text), expect)
+
+    def test_words(self):
+        text = "Test with  many   spaces. and: -,end  "
+        expect = ['test', 'with', 'many', 'spaces.', 'and:', '-,end']
+        self.assertEqual(Words.parse(text), expect)
+
+
 
 if __name__ == "__main__":
     unittest.main()
