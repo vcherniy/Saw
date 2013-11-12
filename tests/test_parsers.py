caed5c719d1f6468f3241b3d69d564b1356d3a4a
Viktor Cherniy <vikchernij@gmail.com>
2013-11-13 00:34:35 +0200

    Full rewrite of parser's tests. - Clean tests (just look on blocks test!) - Rewrite for new logic.

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -5,81 +5,68 @@ from saw.parsers.words import Words
 from saw.parsers.sentences import Sentences
 from saw.parsers.paragraphs import Paragraphs
 
-class Test_Saw(unittest.TestCase):
 
+class TestSaw(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_paragraphs(self):
+        """
+        Explode by: \n
+        """
+        text = "Hi. Mine. \n These items:\n- First item,\n- Second item,   \n - Third item  .\n\nEnded\n"
+        expect = ['Hi. Mine.', ["\n"], 'These items:', ["\n"], '- First item,', ["\n"], '- Second item,', ["\n"],
+                  '- Third item  .', ["\n", "\n"], 'Ended', ["\n"]]
+        self.assertEqual(Paragraphs.parse(text), expect)
+
+    def test_sentences(self):
+        """
+        Explode by: ? ! .
+        """
+        text = '?1 sentence! But what!!WTF   ? .That 12.45 points.   .   Length = 100m..'
+        expect = [['?'], '1 sentence', ['! '], 'But what', ['!', '!'], 'WTF', [' ? ', ' .'], 'That 12',
+                  ['.'], '45 points', ['. ', ' . '], 'Length = 100m', ['.', '.']]
+        self.assertEqual(Sentences.parse(text), expect)
+
+        text = "Test! ! ft.?. start..end ..before and.  ending text"
+        expect = ['Test', ['! ', ' ! '], 'ft', ['.', '?', '. '], 'start', ['.', '.'], 'end',
+                  [' .', '.'], 'before and', ['. '], 'ending text']
+        self.assertEqual(Sentences.parse(text), expect)
+
     def test_blocks(self):
-        single_rules = [',', ':', '=', '+', ';', '*',\
-            '{', '(', '[', ']', ')', '}', '"'\
-            , '- ', "' ", \
-            ' -', " '", '-', "'"]
+        """
+        Explode by: follow list 'rules'
+        """
+        rules = [',', ':', '=', '+', ';', '*', '"', '-', "'", '{', '(', '[', ']', ')', '}', ]
 
-        for rule in single_rules:
-            # striped rule
-            srule = rule.strip()
-            """
-            Test with:
-            space after, space before, space around, another symbols
-            few spaces - to one space
-            """
+        for rule in rules:
+            l_rule = ' ' + rule
+            r_rule = rule + ' '
+            a_rule = ' ' + rule + ' '
+
+            # Test: spaces before, after, around; another symbols; few spaces
             text = "This example{0}   with {0} another. comb!  of {0}spaces".format(rule)
-            if rule in [' -', " '", '-', "'"]:
-                expect = ['This example', [srule], 'with', [srule], 'another. comb!  of ' + rule + 'spaces']
-            else:
-                expect = ['This example', [srule], 'with', [srule], 'another. comb!  of', [srule], 'spaces']
+            expect = ['This example', [r_rule], 'with', [a_rule], 'another. comb!  of', [l_rule], 'spaces']
             self.assertEqual(Blocks.parse(text), expect)
 
-            """
-            Test with:
-            dublicated symbols, another symbol beetwen dublicated (or from this range)
-            symbol in end of string - not empty value
-            """
+            # Test: duplicated symbols, another symbol between it; symbol in the end of string.
             text = "{0}This example{0}{0}   with. {0},{0} another {0}.{0} spaces{0}".format(rule)
-            if rule in ['- ', "' "]:
-                expect = [[srule], 'This example', [srule], [srule], 'with.', [srule], [',', srule], 'another', [srule], '.', [srule], 'spaces', [srule]]
-            elif rule in [' -', " '"]:
-                expect = [srule + 'This example', [srule], [srule], 'with.', [srule, ','], [srule], 'another ' + rule + '.', [srule], 'spaces', [srule]]    
-            elif rule in ['-', "'"]:
-                expect = [rule + 'This example', [srule, srule], 'with.', [srule, ',', srule], 'another ' + rule + '.', [srule], 'spaces', [srule]]
-            else:
-                expect = [[srule], 'This example', [srule, srule], 'with.', [srule, ',', srule], 'another',  [srule], '.', [srule], 'spaces', [srule]]
+            expect = [[rule], 'This example', [rule, r_rule], 'with.', [l_rule, ',', r_rule], 'another',
+                      [l_rule], '.', [r_rule], 'spaces', [rule]]
             self.assertEqual(Blocks.parse(text), expect)
 
-            """Test with:
-            inject nodes to strings
-            """
+            # Test: inject symbols to strings
             text = "Text {0}{0}no{0}br to t{0}{0}mobile".format(rule)
-            if rule in ['- ', "' "]:
-                expect = ['Text', [srule], [srule], 'no', [srule], 'br to t', [srule], [srule], 'mobile']
-            elif rule in [' -', " '"]:
-                expect = ['Text', [srule], srule + 'no' + rule + 'br to t', [srule], srule + 'mobile']
-            elif rule in ['-', "'"]:
-                expect = ['Text ' + rule + rule + 'no' + rule + 'br to t' + rule + rule + 'mobile']
-            else:
-                expect = ['Text', [srule, srule], 'no', [srule], 'br to t', [srule, srule], 'mobile']
+            expect = ['Text', [l_rule, rule], 'no', [rule], 'br to t', [rule, rule], 'mobile']
             self.assertEqual(Blocks.parse(text), expect)
-            
 
     def test_words(self):
+        """
+        Explode by: spaces
+        """
         text = "Test with  many  . spaces. and: -,end  "
         expect = ['Test', 'with', 'many', '.', 'spaces.', 'and:', '-,end']
         self.assertEqual(Words.parse(text), expect)
-
-    def test_sentences(self):
-        text = '?1 sentence! But what!!WTF ? .That 12.45 points. . Lenght = 100m..'
-        expect = [['?'], '1 sentence', ['!'], 'But what', ['!', '!'], 'WTF', ['?'], '.That 12.45 points', ['.'], ['.'], 'Lenght = 100m', ['.', '.']]
-        self.assertEqual(Sentences.parse(text), expect)
-
-        text = "Test! ! ft.?. a..b ..before and. ending text"
-        expect = ['Test', ['!'], ['!'], 'ft', ['.', '?', '.'], 'a..b ..before and', ['.'], 'ending text']
-        self.assertEqual(Sentences.parse(text), expect)
-
-    def test_paragraphs(self):
-        text = "Hi. Hola. \n These items:\n- First item,\n- Second item,   \n - Third item  .\n\nEnded\n"
-        expect = ['Hi. Hola.', ["\n"], 'These items:', ["\n"], '- First item,', ["\n"], '- Second item,', ["\n"], '- Third item  .', ["\n", "\n"], "Ended", ["\n"]]
-        self.assertEqual(Paragraphs.parse(text), expect)
 
 
 if __name__ == "__main__":
