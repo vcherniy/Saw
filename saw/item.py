1fda2a26e1b2fb8a72a6a3f5eb2d9fc99796b3c7
Viktor Cherniy <vikchernij@gmail.com>
2013-11-13 20:27:25 +0200

    Change Item class - _before attribute. - .after_append method - for append item to _after - .after_count - count items in _after

--- saw/item.py
+++ saw/item.py
@@ -7,6 +7,7 @@ class Item:
     _str_dir = dir('')
 
     def __init__(self):
+        self._before = []
         self._after = []
         self._text = ''
         self.children = None
@@ -14,18 +15,25 @@ class Item:
     def after(self, _after):
         self._after = _after
 
+    def after_append(self, item):
+        self._after.append(item)
+
+    def after_count(self):
+        return len(self._after)
+
     def text(self, text):
         self._text = text
 
     def __repr__(self):
         result = self.__dict__
         result.pop("children", None)
+        if not(self._before): result.pop('_before', None)
         if not(self._after): result.pop('_after', None)
         if not(self._text):  result.pop('_text', None)
         return result.__repr__()
 
     def __str__(self):
-        return "%s%s%s" % (str(self.children or ''), self._text, ''.join(self._after))
+        return "%s%s%s%s" % (''.join(self._before), str(self.children or ''), self._text, ''.join(self._after))
 
     # if we call instance with attribute what exists in instance then
     # python will take direct access to children.
