0a729c0022654ee1651a97b6954a728910a28573
Viktor Cherniy <vikchernij@gmail.com>
2014-04-13 19:57:28 +0300

    Extend parser tests

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -49,15 +49,20 @@ class TestSaw(unittest.TestCase):
             expect = [[], 'This example', [r_rule], 'with', [a_rule], 'another. comb!  of', [l_rule], 'spaces', []]
             self.assertEqual(Blocks.parse(text), expect)
 
-            # Test: duplicated symbols, another symbol between it; symbol in the end of string.
-            text = "{0}This example{0}{0}   with. {0},{0} another {0}.{0} spaces{0}".format(rule)
-            expect = [[rule], 'This example', [rule, r_rule], 'with.', [l_rule, ',', r_rule], 'another',
+            # Test: another symbol between it; symbol in the end of string.
+            text = "{0}This example with. {0},{0} another {0}.{0} spaces{0}".format(rule)
+            expect = [[rule], 'This example with.', [l_rule, ',', r_rule], 'another',
                       [l_rule], '.', [r_rule], 'spaces', [rule]]
             self.assertEqual(Blocks.parse(text), expect)
 
+            # Test: duplicated symbols
+            text = "This example{0}{0}   with. {0}{0}no spaces{0} ".format(rule)
+            expect = [[], 'This example', [rule, r_rule], 'with.', [l_rule, rule], 'no spaces', [r_rule]]
+            self.assertEqual(Blocks.parse(text), expect)
+
             # Test: inject symbols to strings
-            text = "Text {0}{0}no{0}br to t{0}{0}mobile".format(rule)
-            expect = [[], 'Text', [l_rule, rule], 'no', [rule], 'br to t', [rule, rule], 'mobile', []]
+            text = "Text no{0}br to t{0}{0}mobile".format(rule)
+            expect = [[], 'Text no', [rule], 'br to t', [rule, rule], 'mobile', []]
             self.assertEqual(Blocks.parse(text), expect)
 
     def test_words(self):
