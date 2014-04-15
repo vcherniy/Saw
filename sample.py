dd35f8aecc0fbaa9a62ce7e8fd0c8a953730b792
Viktor Cherniy <vikchernij@gmail.com>
2014-04-16 00:14:08 +0300

    Any changes for test text file and test exec file

--- sample.py
+++ sample.py
@@ -1,9 +1,7 @@
 from saw.saw import Saw
-from saw.item import Item
 import datetime
+from saw.parsers.blocks import Blocks
 import sys
-
-
 
 with open('test.txt', 'r') as content_file:
     text = content_file.read()
@@ -16,7 +14,6 @@ end_time = datetime.datetime.now()
 
 print 'Time of text load - ' + str(end_time - start_time)
 
-
 print saw.sentences[1:3].blocks[1:]
 print '-------'
 print saw.sentences[-3:].blocks[1:]
@@ -27,13 +24,12 @@ print saw.blocks[12].words[:-2]
 print '-------'
 print saw.blocks[12].words[:-2:2]
 
-
 if str(saw.blocks.words) == str(saw.words):
     print 'str(saw.blocks.words) == str(saw.words)'
 print '-------'
 
 print ' ---- each ---'
-
+"""
 kt = saw.sentences.each().words[:3].get()
 for s in kt:
     print '** ' + str(s)
@@ -71,3 +67,4 @@ print '---------'
 # get first block each paragraphs, save to variable, and lower it and save to another variable
 result = None
 #result2 = saw.paragraphs.each().blocks[0].save_to(result).lower().get()
+"""
