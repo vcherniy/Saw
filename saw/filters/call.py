25b5b86ffd31531543ba6d8797e56670f1b1e8c6
diNard <vikchernij@gmail.com>
2014-08-04 11:18:22 +0200

    Add filter Filter for filtering Node item's chidlren

--- saw/filters/call.py
+++
@@ -1,7 +0,0 @@
-class Call:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def filter(node, func):
-        return node.copy(func)
---
+++ saw/filters/filter.py
@@ -0,0 +1,13 @@
+from saw.node import Node
+
+class Filter:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def filter(node, func):
+        result = Node()
+        for item in node:
+            if func(item):
+                result.append(item)
+        return result
